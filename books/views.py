6cf84087c3c79479c78de36cc21ec731ec61ac16
asdadaversa <peikqudakeus@gmail.com>
2024-04-15 18:37:05 +0300

    implement get_permissions method for Books endpoint

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from books.models import Book
 from books.serializers import BookSerializer
@@ -8,4 +9,13 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = (JWTAuthentication,)
 
+    def get_permissions(self):
+        if self.action in ["create", "update", "destroy"]:
+            permission_classes = [IsAdminUser]
+        elif self.action == "list":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
