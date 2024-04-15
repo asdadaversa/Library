ee57b85c6c710d8df2303c11dcae1dbdd9b67f3b
asdadaversa <peikqudakeus@gmail.com>
2024-04-15 16:21:07 +0300

    implement jwt authentication for project

--- library_service/settings.py
+++ library_service/settings.py
@@ -127,3 +127,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "1000/day", "user": "1000/day"},
+}
