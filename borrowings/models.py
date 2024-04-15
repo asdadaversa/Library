72c4ea4cf48bef8b5b9398290d8ca12599994595
asdadaversa <peikqudakeus@gmail.com>
2024-04-15 19:04:35 +0300

    create borrowings model, register in admin, add app to settings

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from library_service import settings
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ("-borrow_date", )
+
+    def __str__(self) -> str:
+        return (f"borrow date:{self.borrow_date},"
+                f"expected return date: {self.expected_return_date},"
+                )
