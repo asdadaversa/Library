e84560f46ecc9eea91cd835453a6595898601a6a
asdadaversa <peikqudakeus@gmail.com>
2024-04-16 11:01:49 +0300

    update borrowings endpoints, separate endpoints

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -12,19 +11,13 @@ from borrowings.serializers import (
 )
 
 
-class GetBorrowingView(APIView):
-    """ APIVIEW for list and detail endpoint for Borrowing """
-
+class ListCreateBorrowingView(APIView):
     serializer_class = BorrowingReadSerializer
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, pk=None, format=None):
-        if pk:
-            borrowings = get_object_or_404(Borrowing, pk=pk)
-            serializer = BorrowingReadSerializer(borrowings)
-        else:
-            borrowings = Borrowing.objects.all()
-            serializer = BorrowingReadSerializer(borrowings, many=True)
+    def get(self, request, format=None):
+        borrowings = Borrowing.objects.all()
+        serializer = BorrowingReadSerializer(borrowings, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
@@ -36,3 +29,15 @@ class GetBorrowingView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DetailBorrowingView(APIView):
+    """ APIVIEW for list and detail endpoint for Borrowing """
+
+    serializer_class = BorrowingReadSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk, format=None):
+        borrowings = get_object_or_404(Borrowing, pk=pk)
+        serializer = BorrowingReadSerializer(borrowings)
+        return Response(serializer.data, status=status.HTTP_200_OK)
