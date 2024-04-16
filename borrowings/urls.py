9e7858a47f54030cef0d848b94c91cd61c3404f9
asdadaversa <peikqudakeus@gmail.com>
2024-04-16 10:00:32 +0300

    update url

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from borrowings.views import DetailBorrowingView, ListCreateBorrowingView
+from borrowings.views import GetBorrowingView
 
 
 urlpatterns = [
-    path("", ListCreateBorrowingView.as_view(), name="borrowings"),
-    path("<int:pk>/", DetailBorrowingView.as_view(), name="borrowings-detail")
+    path("", GetBorrowingView.as_view(), name="borrowings"),
+    path("<int:pk>/", GetBorrowingView.as_view(), name="borrowings-detail")
 ]
 
 app_name = "borrowings"
