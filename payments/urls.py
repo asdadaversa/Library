10248af9a54bed49c6db7511a5a92ff429f0e63b
asdadaversa <peikqudakeus@gmail.com>
2024-05-01 19:20:41 +0300

    create payment endpoint

---
+++ payments/urls.py
@@ -0,0 +1,13 @@
+from rest_framework import routers
+
+from payments.views import PaymentViewSet
+
+router = routers.DefaultRouter()
+router.register("", PaymentViewSet, basename="payments")
+
+urlpatterns = router.urls
+
+app_name = "payments"
+
+
+
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from payments.serializers import PaymentSerializer
+from payments.models import Payment
+
+
+class PaymentViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated]
