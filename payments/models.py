78716f71f2956615ba88deea80bacbea0f63775b
asdadaversa <peikqudakeus@gmail.com>
2024-05-01 19:19:48 +0300

    create payment model, register in admin

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+from enum import Enum
 
-# Create your models here.
+from django.db import models
+from borrowings.models import Borrowing
+
+
+class PaymentStatus(Enum):
+    PENDING = "PENDING"
+    PAID = "PAID"
+
+
+class PaymentType(Enum):
+    PAYMENT = "PAYMENT"
+    FINE = "FINE"
+
+
+class Payment(models.Model):
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE, related_name="payments")
+    status = models.CharField(
+        max_length=10, choices=[(payment.value, payment.name) for payment in PaymentStatus]
+    )
+    type = models.CharField(
+        max_length=10, choices=[(tag.value, tag.name) for tag in PaymentType]
+    )
+    session_url = models.URLField(blank=True, null=True)
+    session = models.CharField(max_length=63)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ["borrowing"]
+        verbose_name = "Payment"
+        verbose_name_plural = "Payments"
+
+    def __str__(self) -> str:
+        return f"{self.borrowing}, status: {self.status}, total: {self.money_to_pay}"
